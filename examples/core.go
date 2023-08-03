723b6c55eeff2a6eccc6ff8bb677fe0ef2a1a379
Jason Hatton <jason@hatton.io>
2023-08-03 18:52:20 -0500

    Add Camera3DMode example

--- examples/core.go
+++ examples/core.go
@@ -1,23 +1,25 @@
 package core
 
-import rl "github.com/gen2brain/raylib-go/raylib"
-import "image/color"
+import (
+	"image/color"
 
-var buttonColors map[int32]color.RGBA = map[int32]color.RGBA{
-	rl.MouseLeftButton:    rl.Maroon,
-	rl.MouseMiddleButton:  rl.Lime,
-	rl.MouseRightButton:   rl.DarkBlue,
-	rl.MouseSideButton:    rl.Purple,
-	rl.MouseExtraButton:   rl.Yellow,
-	rl.MouseForwardButton: rl.Orange,
-	rl.MouseBackButton:    rl.Beige,
-}
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
 
 const screenWidth = 800
 const screenHeight = 450
 const framesPerSecond = 60
 
 func MouseInput() {
+	var buttonColors map[int32]color.RGBA = map[int32]color.RGBA{
+		rl.MouseLeftButton:    rl.Maroon,
+		rl.MouseMiddleButton:  rl.Lime,
+		rl.MouseRightButton:   rl.DarkBlue,
+		rl.MouseSideButton:    rl.Purple,
+		rl.MouseExtraButton:   rl.Yellow,
+		rl.MouseForwardButton: rl.Orange,
+		rl.MouseBackButton:    rl.Beige,
+	}
 	rl.InitWindow(screenWidth, screenHeight, "raylib-go [core] example - mouse input")
 	defer rl.CloseWindow()
 	ballPosition := rl.Vector2{X: 0, Y: 0}
@@ -38,9 +40,40 @@ func MouseInput() {
 		{
 			rl.BeginDrawing()
 			defer rl.EndDrawing()
+
 			rl.ClearBackground(rl.RayWhite)
 			rl.DrawCircleV(ballPosition, 40, ballColor)
 			rl.DrawText("move ball with mouse and click mouse button to change color", 10, 10, 20, rl.DarkGray)
 		}
+	}
+}
+
+func Camera3DMode() {
+	rl.InitWindow(screenWidth, screenHeight, "raylib-go [core] example - 3D camera mode")
+	defer rl.CloseWindow()
+	position := rl.NewVector3(0, 10, 10)
+	target := rl.NewVector3(0, 0, 0)
+	up := rl.NewVector3(0, 1, 0)
+	var fovy float32 = 45.0
+	proj := rl.CameraPerspective
+	camera := rl.NewCamera3D(position, target, up, fovy, proj)
+	cubePosition := rl.NewVector3(0, 0, 0)
+	rl.SetTargetFPS(framesPerSecond)
+
+	for !rl.WindowShouldClose() {
+		func() {
+			rl.BeginDrawing()
+			defer rl.EndDrawing()
+			rl.ClearBackground(rl.RayWhite)
+			func() {
+				rl.BeginMode3D(camera)
+				defer rl.EndMode3D()
+				rl.DrawCube(cubePosition, 2, 2, 2, rl.Red)
+				rl.DrawCubeWires(cubePosition, 2, 2, 2, rl.Maroon)
+				rl.DrawGrid(10, 10)
+			}()
+			rl.DrawText("Welcome to the third dimension!", 10, 40, 20, rl.DarkGray)
+			rl.DrawFPS(10, 10)
+		}()
 	}
 }
--- raygo.go
+++ raygo.go
@@ -3,5 +3,5 @@ package main
 import core "github.com/jason-h-35/raygo/examples"
 
 func main() {
-	core.MouseInput()
+	core.Camera3DMode()
 }
