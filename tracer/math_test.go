8575ef8daa085344218311c62f14d18da332d06e
Jason Hatton <jason@hatton.io>
2023-08-08 18:16:02 -0500

    add more tests

--- tracer/math_test.go
+++ tracer/math_test.go
@@ -3,8 +3,31 @@ package tracer
 import "testing"
 
 func Test_NewTuple(t *testing.T) {
-	tuple := NewTuple(4.3, -4.2, 3.1, 1.0)
-	if tuple.X != 4.3 || tuple.Y != -4.2 || tuple.Z != 3.1 || tuple.W != 1.0 {
-		t.Error("tuple fields incorrect")
+	expect := Tuple{X: 4.3, Y: -4.2, Z: 3.1, W: 1}
+	got := NewTuple(4.3, -4.2, 3.1, 1)
+	if got != expect {
+		t.Errorf("expected: %v. got: %v", expect, got)
+	}
+}
+
+func Test_Point(t *testing.T) {
+	expect := Tuple{X: 4.3, Y: -4.2, Z: 3.1, W: 1}
+	got := Point(4.3, -4.2, 3.1)
+	if got != expect {
+		t.Errorf("expected: %v. got: %v", expect, got)
+	}
+	if !got.IsPoint() || !expect.IsPoint() {
+		t.Errorf("expected IsPoint() == true for both bare struct and New method.")
+	}
+}
+
+func Test_Vector(t *testing.T) {
+	expect := Tuple{X: 4.3, Y: -4.2, Z: 3.1, W: 0}
+	got := Vector(4.3, -4.2, 3.1)
+	if got != expect {
+		t.Errorf("expected: %v. got: %v", expect, got)
+	}
+	if !got.IsVector() || !expect.IsVector() {
+		t.Errorf("expected IsVector() == true for both bare struct and New method.")
 	}
 }
