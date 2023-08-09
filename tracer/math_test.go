e675d18fe910ce76db7cadf21b8ef1b5bd76cad5
Jason Hatton <jason@hatton.io>
2023-08-09 01:49:47 -0500

    Test_Plus() and minor formatting stuff

--- tracer/math_test.go
+++ tracer/math_test.go
@@ -38,12 +38,29 @@ func Test_Equals(t *testing.T) {
 	diff := Point(1, 2, 3)
 	zero := NewTuple(0, 0, 0, 0)
 	if !same1.Equals(same2) || !same2.Equals(same1) {
-		t.Errorf("expected %v Equals %v.", same1, same2)
+		t.Errorf("expected %v Equals %v", same1, same2)
 	}
 	if same2.Equals(diff) {
-		t.Errorf("expected %v Not Equals %v.", same2, diff)
+		t.Errorf("expected %v Not Equals %v", same2, diff)
 	}
 	if zero.Equals(same1) || zero.Equals(same2) || zero.Equals(diff) {
-		t.Errorf("zero tuple shouldn't equal %v, %v, or %v.", same1, same2, diff)
+		t.Errorf("zero tuple shouldn't equal %v, %v, or %v", same1, same2, diff)
+	}
+}
+
+func Test_Plus(t *testing.T) {
+	point := Point(1, 2, 3)
+	vector := Vector(1, 2, 3)
+	test1 := point.Plus(vector)
+	test2 := vector.Plus(point)
+	test3 := vector.Plus(vector)
+	if !test1.Equals(test2) {
+		t.Errorf("expected Plus to be commutative: %v would Equal %v", test1, test2)
+	}
+	if !test1.IsPoint() || !test2.IsPoint() {
+		t.Errorf("expected a Vector Plus a Point would be a Point: %v", test1)
+	}
+	if !test3.IsVector() {
+		t.Errorf("expected a Vector Plus a Vector would be a Vector: %v", test3)
 	}
 }
