18b2c82f560397fcd23554ac3a27568fffa46a6f
Jason Hatton <jason@hatton.io>
2023-08-10 17:17:12 -0500

    putting it together ch1

--- raygo.go
+++ raygo.go
@@ -1,7 +1,23 @@
 package main
 
-import core "github.com/jason-h-35/raygo/examples"
+import "github.com/jason-h-35/raygo/tracer"
+
+type Projectile struct {
+	position tracer.Tuple
+	velocity tracer.Tuple
+}
+
+type Environment struct {
+	gravity tracer.Tuple
+	wind    tracer.Tuple
+}
+
+func tick(env Environment, proj Projectile) Projectile {
+	position := proj.position.Plus(proj.velocity)
+	velocity := proj.velocity.Plus(env.gravity).Plus(env.wind)
+	return Projectile{position, velocity}
+}
 
 func main() {
-	core.BunnyMark()
+
 }
