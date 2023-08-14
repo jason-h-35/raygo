476cc59b553005bd5893c65dbb1ef6e5292a3e07
Jason Hatton <jason@hatton.io>
2023-08-14 18:08:16 -0500

    rename Blend to match book

--- tracer/canvas/canvas.go
+++ tracer/canvas/canvas.go
@@ -14,8 +14,17 @@ type Color struct {
 var Black Color = NewColor(0, 0, 0)
 var White Color = NewColor(1, 1, 1)
 
+var eps float64 = 1.e-5
+
 func NewColor(R, G, B float64) Color {
 	return Color{R, G, B}
+}
+
+func abs(x float64) float64 {
+	if x < 0 {
+		return -x
+	}
+	return x
 }
 
 func (c1 Color) Equals(c2 Color) bool {
@@ -43,6 +52,6 @@ func (c Color) Times(f float64) Color {
 	return NewColor(f*c.R, f*c.G, f*c.B)
 }
 
-func (c1 Color) Blend(c2 Color) Color {
+func (c1 Color) Hadamard(c2 Color) Color {
 	return NewColor(c1.R*c1.R, c1.G*c2.G, c1.B*c2.B)
 }
