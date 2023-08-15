b5d72d101b9664f8d9dabd42a27968741c87bead
Jason Hatton <jason@hatton.io>
2023-08-15 02:40:48 -0500

    initial PPM write implementation

--- tracer/canvas/canvas.go
+++ tracer/canvas/canvas.go
@@ -1,15 +1,29 @@
 package canvas
 
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
 type Canvas struct {
 	image [][]Color
 }
 
 func NewCanvas(width, height int) Canvas {
-	image := make([][]Color, width)
+	image := make([][]Color, width, width)
 	for i := range image {
-		image[i] = make([]Color, height)
+		image[i] = make([]Color, height, height)
 	}
 	return Canvas{image}
+}
+
+func (c *Canvas) Width() int {
+	return len(c.image)
+}
+
+func (c *Canvas) Height() int {
+	return len(c.image[0]) // how to check if all sub-arrays are the same length?
 }
 
 func (c *Canvas) WritePixel(x int, y int, color Color) {
@@ -20,4 +34,18 @@ func (c *Canvas) WritePixel(x int, y int, color Color) {
 func (c *Canvas) ReadPixel(x, y int) Color {
 	canvas := *c
 	return canvas.image[x][y]
+}
+
+func (c *Canvas) ToPPM() string {
+	colorRepMax := 255.0
+	ppmHeader := fmt.Sprintf("\nP3\n%v %v\n%v\n", c.Width, c.Height, colorMax)
+	var b strings.Builder
+	for _, row := range c.image {
+		for _, color := range row {
+			colorRep := string(int(math.Round(color.Times(colorRepMax))))
+			b.WriteString(colorRep)
+		}
+	}
+	b.WriteRune('\n')
+	return b.String()
 }
