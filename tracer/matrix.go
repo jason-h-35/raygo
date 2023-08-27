74227118b1efd3029fce0af8e197c4aa03c56b19
Jason Hatton <jason@hatton.io>
2023-08-27 18:20:54 -0500

    field naming

--- tracer/matrix.go
+++ tracer/matrix.go
@@ -1,27 +1,27 @@
 package tracer
 
 type Mat4 struct {
-	data [4][4]float64
+	arr [4][4]float64
 }
 
 type Mat3 struct {
-	data [3][3]float64
+	arr [3][3]float64
 }
 
 type Mat2 struct {
-	data [2][2]float64
+	arr [2][2]float64
 }
 
 func NewMat2(a, b, c, d float64) Mat2 {
-	return Mat2{data: [2][2]float64{{a, b}, {c, d}}}
+	return Mat2{arr: [2][2]float64{{a, b}, {c, d}}}
 }
 
 func NewMat3(a, b, c, d, e, f, g, h, i float64) Mat3 {
-	return Mat3{data: [3][3]float64{{a, b, c}, {d, e, f}, {g, h, i}}}
+	return Mat3{arr: [3][3]float64{{a, b, c}, {d, e, f}, {g, h, i}}}
 }
 
 func NewMat4(aa, ab, ac, ad, ba, bb, bc, bd, ca, cb, cc, cd, da, db, dc, dd float64) Mat4 {
-	return Mat4{data: [4][4]float64{{aa, ab, ac, ad}, {ba, bb, bc, bd}, {ca, cb, cc, cd}, {da, db, dc, dd}}}
+	return Mat4{arr: [4][4]float64{{aa, ab, ac, ad}, {ba, bb, bc, bd}, {ca, cb, cc, cd}, {da, db, dc, dd}}}
 }
 
 func (m *Mat4) At(i, j int) float64 {
