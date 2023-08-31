9a95c8266392c8ae7746081b4a8fef3f354b4b5f
Jason Hatton <jason@hatton.io>
2023-08-31 00:36:11 -0500

    fix tests for new slice constructors

--- tracer/matrix_test.go
+++ tracer/matrix_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func Test_NewMat2(t *testing.T) {
-	m := NewMat2(-3, 5, 1, -2)
+	m := NewMat2([]float64{-3, 5, 1, -2})
 	table := []MatVal{
 		{0, 0, -3}, {0, 1, 5}, {1, 0, 1}, {1, 1, -2},
 	}
@@ -19,7 +19,7 @@ func Test_NewMat2(t *testing.T) {
 }
 
 func Test_NewMat3(t *testing.T) {
-	m := NewMat3(-3, 5, 0, 1, -2, -7, 0, 1, 1)
+	m := NewMat3([]float64{-3, 5, 0, 1, -2, -7, 0, 1, 1})
 	table := []MatVal{{0, 0, -3}, {1, 1, -2}, {2, 2, 1}}
 	for _, v := range table {
 		result := m.At(v.i, v.j)
@@ -32,7 +32,7 @@ func Test_NewMat3(t *testing.T) {
 }
 
 func Test_NewMat4(t *testing.T) {
-	m := NewMat4(1, 2, 3, 4, 5.5, 6.5, 7.5, 8.5, 9, 10, 11, 12, 13.5, 14.5, 15.5, 16.5)
+	m := NewMat4([]float64{1, 2, 3, 4, 5.5, 6.5, 7.5, 8.5, 9, 10, 11, 12, 13.5, 14.5, 15.5, 16.5})
 	table := []MatVal{
 		{0, 0, 1.0}, {0, 3, 4.0}, {1, 0, 5.5}, {1, 2, 7.5},
 		{2, 2, 11.0}, {3, 0, 13.5}, {3, 2, 15.5},
