3fffc0cf86d06ef3c9f17dbed15117b65c6c85fd
Jason Hatton <jason@hatton.io>
2023-08-31 00:15:08 -0500

    rename arr to vals

--- tracer/matrix.go
+++ tracer/matrix.go
@@ -1,15 +1,17 @@
 package tracer
 
+// https://github.com/golang/go/issues/44253
+
 type Mat4 struct {
-	arr [4][4]float64
+	vals [4][4]float64
 }
 
 type Mat3 struct {
-	arr [3][3]float64
+	vals [3][3]float64
 }
 
 type Mat2 struct {
-	arr [2][2]float64
+	vals [2][2]float64
 }
 
 type MatVal struct {
@@ -23,33 +25,33 @@ func NewMatVal(i int, j int, val float64) MatVal {
 }
 
 func NewMat2(a, b, c, d float64) Mat2 {
-	return Mat2{arr: [2][2]float64{{a, b}, {c, d}}}
+	return Mat2{vals: [2][2]float64{{a, b}, {c, d}}}
 }
 
 func NewMat3(a, b, c, d, e, f, g, h, i float64) Mat3 {
-	return Mat3{arr: [3][3]float64{{a, b, c}, {d, e, f}, {g, h, i}}}
+	return Mat3{vals: [3][3]float64{{a, b, c}, {d, e, f}, {g, h, i}}}
 }
 
 func NewMat4(aa, ab, ac, ad, ba, bb, bc, bd, ca, cb, cc, cd, da, db, dc, dd float64) Mat4 {
-	return Mat4{arr: [4][4]float64{{aa, ab, ac, ad}, {ba, bb, bc, bd}, {ca, cb, cc, cd}, {da, db, dc, dd}}}
+	return Mat4{vals: [4][4]float64{{aa, ab, ac, ad}, {ba, bb, bc, bd}, {ca, cb, cc, cd}, {da, db, dc, dd}}}
 }
 
 func (m *Mat4) At(i, j int) float64 {
-	return m.arr[i][j]
+	return m.vals[i][j]
 }
 
 func (m *Mat3) At(i, j int) float64 {
-	return m.arr[i][j]
+	return m.vals[i][j]
 }
 
 func (m *Mat2) At(i, j int) float64 {
-	return m.arr[i][j]
+	return m.vals[i][j]
 }
 
 func (m1 *Mat4) Equals(m2 *Mat4) bool {
-	for i := range m1.arr {
-		for j := range m1.arr {
-			if abs(m1.arr[i][j]-m2.arr[i][j]) > eps {
+	for i := range m1.vals {
+		for j := range m1.vals {
+			if abs(m1.vals[i][j]-m2.vals[i][j]) > eps {
 				return false
 			}
 		}
