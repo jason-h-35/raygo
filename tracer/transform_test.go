22e72398b5683ecd7c59d226858b71b189456792
Jason Hatton <jason@hatton.io>
2023-09-27 06:34:43 -0700

    Scale tests

--- tracer/transform_test.go
+++ tracer/transform_test.go
@@ -13,6 +13,10 @@ func TestTranslate(t *testing.T) {
 		{I4.Translate(5, -3, 2), Point(-3, 4, 5), Point(2, 1, 7)},
 		{I4.Translate(5, -3, 2).Inverse(), Point(-3, 4, 5), Point(-8, 7, 3)},
 		{I4.Translate(5, -3, 2), Vector(-3, 4, 5), Vector(-3, 4, 5)},
+		{I4.Scale(2, 3, 4), Point(-4, 6, 8), Point(-8, 18, 32)},
+		{I4.Scale(2, 3, 4), Vector(-4, 6, 8), Vector(-8, 18, 32)},
+		{I4.Scale(2, 3, 4).Inverse(), Vector(-4, 6, 8), Vector(-2, 2, 2)},
+		{I4.Scale(-1, 1, 1), Point(2, 3, 4), Point(-2, 3, 4)},
 	}
 	for _, row := range data {
 		result := row.transform.TimesTuple(row.pt)
