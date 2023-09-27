718513dcb6fa4e80f6ebd04af0e29981149fcce3
Jason Hatton <jason@hatton.io>
2023-09-27 10:00:04 -0700

    X,Y,Z Rotate tests

--- tracer/transform_test.go
+++ tracer/transform_test.go
@@ -18,8 +18,13 @@ func TestTransform(t *testing.T) {
 		{I4.Scale(2, 3, 4), Vector(-4, 6, 8), Vector(-8, 18, 32)},
 		{I4.Scale(2, 3, 4).Inverse(), Vector(-4, 6, 8), Vector(-2, 2, 2)},
 		{I4.Scale(-1, 1, 1), Point(2, 3, 4), Point(-2, 3, 4)},
-		{I4.RotateX(math.Pi / 4.0), Point(0, 1, 0), Point(0, math.Sqrt(2)/2.0, math.Sqrt(2)/2.0)},
-		{I4.RotateX(math.Pi / 2.0), Point(0, 1, 0), Point(0, 0, 1)},
+		{I4.RotateX(math.Pi / 4.), Point(0, 1, 0), Point(0, math.Sqrt(2)/2., math.Sqrt(2)/2.)},
+		{I4.RotateX(math.Pi / 2.), Point(0, 1, 0), Point(0, 0, 1)},
+		{I4.RotateX(math.Pi / 4.).Inverse(), Point(0, 1, 0), Point(0, math.Sqrt(2)/2., -math.Sqrt(2)/2.)},
+		{I4.RotateY(math.Pi / 4.), Point(0, 0, 1), Point(math.Sqrt(2)/2., 0, math.Sqrt(2)/2.)},
+		{I4.RotateY(math.Pi / 2.), Point(0, 0, 1), Point(1, 0, 0)},
+		{I4.RotateZ(math.Pi / 4.), Point(0, 1, 0), Point(-math.Sqrt(2)/2., math.Sqrt(2)/2., 0)},
+		{I4.RotateZ(math.Pi / 2.), Point(0, 1, 0), Point(-1, 0, 0)},
 	}
 	for _, row := range data {
 		result := row.transform.TimesTuple(row.pt)
