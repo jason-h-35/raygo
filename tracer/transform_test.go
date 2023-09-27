36e600c9213bdc698d26e30711828cc16d0886de
Jason Hatton <jason@hatton.io>
2023-09-27 06:21:24 -0700

    3 translate tests

--- tracer/transform_test.go
+++ tracer/transform_test.go
@@ -4,12 +4,20 @@ import (
 	"testing"
 )
 
-func TestTranslation(t *testing.T) {
-	transform := I4.Translate(5, -3, 2)
-	p := Point(-3, 4, 5)
-	result := transform.Times(p)
-	expect := Point(2, 1, 7)
-	if !result.Equal(expect) {
-		t.Errorf("blah blah blah")
+func TestTranslate(t *testing.T) {
+	data := []struct {
+		transform Mat4
+		pt        Tuple
+		expect    Tuple
+	}{
+		{I4.Translate(5, -3, 2), Point(-3, 4, 5), Point(2, 1, 7)},
+		{I4.Translate(5, -3, 2).Inverse(), Point(-3, 4, 5), Point(-8, 7, 3)},
+		{I4.Translate(5, -3, 2), Vector(-3, 4, 5), Vector(-3, 4, 5)},
+	}
+	for _, row := range data {
+		result := row.transform.TimesTuple(row.pt)
+		if result != row.expect {
+			t.Errorf("Point %v should have translated to %v but was %v", row.pt, row.expect, result)
+		}
 	}
 }
