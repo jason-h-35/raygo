37f2f0210335140491085c5483f71ef02c1fa558
Jason Hatton <jason@hatton.io>
2023-09-27 06:50:21 -0700

    RotateX, RotateY, RotateZ

--- tracer/transform.go
+++ tracer/transform.go
@@ -26,11 +26,33 @@ func (m Mat4) Scale(x, y, z float64) Mat4 {
 	return T.Times(m)
 }
 
-func (m Mat4) Rotate(rad float64) Mat4 {
+func (m Mat4) RotateX(rad float64) Mat4 {
 	f := []float64{
 		1, 0, 0, 0,
 		0, math.Cos(rad), -math.Sin(rad), 0,
 		0, math.Sin(rad), math.Cos(rad), 0,
+		0, 0, 0, 1,
+	}
+	T := NewMat4(f)
+	return T.Times(m)
+}
+
+func (m Mat4) RotateY(rad float64) Mat4 {
+	f := []float64{
+		math.Cos(rad), 0, math.Sin(rad), 0,
+		0, 1, 0, 0,
+		-math.Sin(rad), 0, math.Cos(rad), 0,
+		0, 0, 0, 1,
+	}
+	T := NewMat4(f)
+	return T.Times(m)
+}
+
+func (m Mat4) RotateZ(rad float64) Mat4 {
+	f := []float64{
+		math.Cos(rad), -math.Sin(rad), 0, 0,
+		math.Sin(rad), math.Cos(rad), 0, 0,
+		0, 0, 1, 0,
 		0, 0, 0, 1,
 	}
 	T := NewMat4(f)
