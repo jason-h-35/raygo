e11355b2d5c6a3ea9ccff13c6c9357a902815add
Jason Hatton <jason@hatton.io>
2024-12-03 02:25:18 -0600

    WIP: convert matrix.go to generics

--- tracer/matrix_test.go
+++ tracer/matrix_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func Test_NewMat2(t *testing.T) {
-	m := NewMat2([]float64{-3, 5, 1, -2})
+	m := NewMat[2]([]float64{-3, 5, 1, -2})
 	table := []MatVal{
 		{0, 0, -3}, {0, 1, 5}, {1, 0, 1}, {1, 1, -2},
 	}
@@ -19,7 +19,7 @@ func Test_NewMat2(t *testing.T) {
 }
 
 func Test_NewMat3(t *testing.T) {
-	m := NewMat3([]float64{-3, 5, 0, 1, -2, -7, 0, 1, 1})
+	m := NewMat[3]([]float64{-3, 5, 0, 1, -2, -7, 0, 1, 1})
 	table := []MatVal{{0, 0, -3}, {1, 1, -2}, {2, 2, 1}}
 	for _, v := range table {
 		result := m.At(v.i, v.j)
@@ -32,7 +32,7 @@ func Test_NewMat3(t *testing.T) {
 }
 
 func Test_NewMat4(t *testing.T) {
-	m := NewMat4([]float64{1, 2, 3, 4, 5.5, 6.5, 7.5, 8.5, 9, 10, 11, 12, 13.5, 14.5, 15.5, 16.5})
+	m := NewMat[4]([]float64{1, 2, 3, 4, 5.5, 6.5, 7.5, 8.5, 9, 10, 11, 12, 13.5, 14.5, 15.5, 16.5})
 	table := []MatVal{
 		{0, 0, 1.0}, {0, 3, 4.0}, {1, 0, 5.5}, {1, 2, 7.5},
 		{2, 2, 11.0}, {3, 0, 13.5}, {3, 2, 15.5},
@@ -47,32 +47,32 @@ func Test_NewMat4(t *testing.T) {
 }
 
 func Test_Mat4Equals(t *testing.T) {
-	m1 := NewMat4([]float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2})
-	m2 := NewMat4([]float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2})
+	m1 := NewMat[4]([]float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2})
+	m2 := NewMat[4]([]float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2})
 	if !m1.Equals(m2) {
 		t.Errorf("%v should equal %v", m1, m2)
 	}
 }
 
 func Test_Mat3Equals(t *testing.T) {
-	m1 := NewMat3([]float64{1, 2, 3, 4, 5, 6, 7, 8, 9})
-	m2 := NewMat3([]float64{1, 2, 3, 4, 5, 6, 7, 8, 9})
+	m1 := NewMat[3]([]float64{1, 2, 3, 4, 5, 6, 7, 8, 9})
+	m2 := NewMat[3]([]float64{1, 2, 3, 4, 5, 6, 7, 8, 9})
 	if !m1.Equals(m2) {
 		t.Errorf("%v should equal %v", m1, m2)
 	}
 }
 
 func Test_Mat2Equals(t *testing.T) {
-	m1 := NewMat2([]float64{1, 2, 3, 4})
-	m2 := NewMat2([]float64{1, 2, 3, 4})
+	m1 := NewMat[2]([]float64{1, 2, 3, 4})
+	m2 := NewMat[2]([]float64{1, 2, 3, 4})
 	if !m1.Equals(m2) {
 		t.Errorf("%v should equal %v", m1, m2)
 	}
 }
 
 func Test_Mat4TimesMat4(t *testing.T) {
-	a := NewMat4([]float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2})
-	b := NewMat4([]float64{-2, 1, 2, 3, 3, 2, 1, -1, 4, 3, 6, 5, 1, 2, 7, 8})
+	a := NewMat[4]([]float64{1, 2, 3, 4, 5, 6, 7, 8, 9, 8, 7, 6, 5, 4, 3, 2})
+	b := NewMat[4]([]float64{-2, 1, 2, 3, 3, 2, 1, -1, 4, 3, 6, 5, 1, 2, 7, 8})
 	expect := NewMat4([]float64{20, 22, 50, 48, 44, 54, 114, 108, 40, 58, 110, 102, 16, 26, 46, 42})
 	result := a.Times(b)
 	if !result.Equals(expect) {
