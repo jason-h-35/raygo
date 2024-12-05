1d07677bfe1e8ca941d915eb91a49681bf765aa3
Jason Hatton <jason@hatton.io>
2024-12-05 02:27:38 -0600

    WIP: func name typo

--- tracer/matrix_test.go
+++ tracer/matrix_test.go
@@ -31,7 +31,7 @@ func Test_NewMat3(t *testing.T) {
 
 }
 
-func Test_NewMat(t *testing.T) {
+func Test_NewMat4(t *testing.T) {
 	m := NewMat[Size4]([]float64{1, 2, 3, 4, 5.5, 6.5, 7.5, 8.5, 9, 10, 11, 12, 13.5, 14.5, 15.5, 16.5})
 	table := []MatVal{
 		{0, 0, 1.0}, {0, 3, 4.0}, {1, 0, 5.5}, {1, 2, 7.5},
