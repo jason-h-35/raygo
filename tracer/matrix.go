c1a5cde329ff7e8f527af33c02f06e704415ceaa
Jason Hatton <jason@hatton.io>
2024-12-05 00:59:36 -0600

    WIP: moving to generic Mat

--- tracer/matrix.go
+++ tracer/matrix.go
@@ -1,5 +1,9 @@
 package tracer
 
+import (
+	"fmt"
+)
+
 type Matrix[T int] interface {
 	At(i, j int) float64
 	Equals(other Matrix[T]) bool
@@ -8,7 +12,8 @@ type Matrix[T int] interface {
 }
 
 type Mat[T int] struct {
-	vals [T][T]float64
+	vals [][]float64
+	size T
 }
 
 type MatVal struct {
@@ -17,13 +22,23 @@ type MatVal struct {
 	val float64
 }
 
-var I2 = NewMat[2]([]float64{1, 0, 0, 1})
-var I3 = NewMat[3]([]float64{1, 0, 0, 0, 1, 0, 0, 0, 1})
-var I4 = NewMat[4]([]float64{1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1})
+type Size2 int
+type Size3 int
+type Size4 int
 
-var Z2 = NewMat[2](make([]float64, 4))
-var Z3 = NewMat[3](make([]float64, 9))
-var Z4 = NewMat[4](make([]float64, 16))
+const (
+	_2 Size2 = 2
+	_3 Size3 = 3
+	_4 Size4 = 4
+)
+
+var I2 = NewMat[Size2]([]float64{1, 0, 0, 1})
+var I3 = NewMat[Size3]([]float64{1, 0, 0, 0, 1, 0, 0, 0, 1})
+var I4 = NewMat[Size4]([]float64{1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1})
+
+var Z2 = NewMat[Size2](make([]float64, 4))
+var Z3 = NewMat[Size3](make([]float64, 9))
+var Z4 = NewMat[Size4](make([]float64, 16))
 
 func NewMatVal(i int, j int, val float64) MatVal {
 	return MatVal{i, j, val}
@@ -36,13 +51,23 @@ func NewMat[T int](a []float64) Mat[T] {
 		panic(fmt.Sprintf("Mat%d needs %d elements", size, expected))
 	}
 
-	m := Mat[T]{vals: [T][T]float64{}}
+	// Initialize the 2D slice
+	vals := make([][]float64, int(size))
+	for i := range vals {
+		vals[i] = make([]float64, int(size))
+	}
+
+	// Fill the values
 	for i := 0; i < int(size); i++ {
 		for j := 0; j < int(size); j++ {
-			m.vals[i][j] = a[i*int(size)+j]
+			vals[i][j] = a[i*int(size)+j]
 		}
 	}
-	return m
+
+	return Mat[T]{
+		vals: vals,
+		size: size,
+	}
 }
 
 func (m Mat[T]) At(i, j int) float64 {
@@ -103,10 +128,6 @@ func (a Mat[T]) Transpose() Mat[T] {
 	return NewMat[T](result)
 }
 
-func (a Mat[T]) Determinant() float64 {
-	return a.vals[0][0]*a.vals[1][1] - a.vals[0][1]*a.vals[1][0]
-}
-
 func SubMat[T, U int](a Mat[T], is, js int) Mat[U] {
 	s := make([]float64, 0)
 	for i, row := range a.vals {
@@ -140,15 +161,19 @@ func Cofactor[T int](a Mat[T], is, js int) float64 {
 
 func (a Mat[T]) Determinant() float64 {
 	var size T
-	if size == 2 {
+	switch size {
+	case 2:
 		return a.vals[0][0]*a.vals[1][1] - a.vals[0][1]*a.vals[1][0]
+	case 3, 4:
+		det, i := 0.0, 0
+		for j, val := range a.vals[i] {
+			det += val * Cofactor(a, i, j)
+		}
+		return det
+	default:
+		panic(fmt.Sprintf("Determinant not implemented for size %d", size))
 	}
 
-	det, i := 0.0, 0
-	for j, val := range a.vals[i] {
-		det += val * Cofactor(a, i, j)
-	}
-	return det
 }
 
 func (a Mat[T]) CanInverse() bool {
